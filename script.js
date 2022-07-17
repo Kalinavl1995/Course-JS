079c50f5e753fa5b5a775d044e9814487be7cdf1
Kalinin Vladislav <Kalinavl1995@gmail.com>
2022-07-17 21:33:28 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const a = prompt('Один из последних просмотренных фильмов ?', ''),
+      b = prompt('На сколько оцените его от 1 до 10 ?', ''),
+      c = prompt('Один из последних просмотренных фильмов ?', ''),
+      d = prompt('На сколько оцените его от 1 до 10 ?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+
+
+
+
+
+console.log(personalMovieDB.movies);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
